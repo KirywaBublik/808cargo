9da02f04ca8157b4c832c5ded184b9ccdef422a5
Kirill Anisimov <kirillanisimov1337@yandex.ru>
2024-02-12 14:42:04 +0300

    Finish footer, allias, eslint&prettier

--- client/src/shared/icons/index.ts
+++
@@ -1,3 +0,0 @@
-import logo from "./logo.svg";
-import burger from "./burger.svg";
-export { logo, burger };
---
+++ client/src/widgets/Header/model/constants.ts
@@ -0,0 +1,17 @@
+export const headerList = [
+	{
+		id: 0,
+		name: 'г л а в н а я',
+		scroll: '/',
+	},
+	{
+		id: 1,
+		name: 'к а т а л о г',
+		scroll: 'catalog',
+	},
+	{
+		id: 2,
+		name: 'к о н т а к т ы',
+		scroll: 'contacts',
+	},
+];
