47287f6a3145259c9e33ccfb6df362ff8f9bb9c2
Str <gostlix20201@gmail.com>
2024-02-27 23:28:01 +0300

    feat: Добавил локализацию на мадильное окно и на страницу контактов

--- client/src/pages/ContactPage/ui/ContactPage.tsx
+++ client/src/pages/ContactPage/ui/ContactPage.tsx
@@ -1,30 +1,18 @@
 import { HrefBlock } from "@/feature/hrefBlock";
 import { SubscribeForm } from "@/feature/subscribeForm";
-import { images } from "./images";
+import { useTranslation } from "react-i18next";
 
 const ContactPage = () => {
+  const { t } = useTranslation();
   return (
     <div className="h-dvh max-w-[1170px] mx-auto w-full">
-      <HrefBlock />
+      <HrefBlock pathes={["Main", "Contacts"]} />
       <div className="flex justify-center items-center flex-col text-5xl">
-        <h1 className="mb-4">Контакты</h1>
-        <div className="text-lg text-center">
-          <p>
-            Если у вас возникли вопросы, свяжитесь
-          </p>
-          <p>
-            с нами любым удобным для вас способом:
-          </p>
-        </div>
-        <div className="flex gap-24 mt-4 mb-4">
-          {images.map((elem, index) => (
-            <img
-              width={40}
-              key={index}
-              src={elem.src}
-            />
-          ))}
-        </div>
+        <h1 className="mb-4">{t("Contacts")}</h1>
+        <p className="text-base text-center">
+          {t("ЗАКАЖИТЕ ЗВОНОК")} <br />{" "}
+          {t("ПРЯМО СЕЙЧАС")}
+        </p>
       </div>
       <div className="w-[400px] mx-auto">
         <SubscribeForm />
