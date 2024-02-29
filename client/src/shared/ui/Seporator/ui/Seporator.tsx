58a53f3723e12541738b05ffbaec4f647606a6a0
Str <gostlix20201@gmail.com>
2024-02-29 16:58:13 +0300

    fix: исправил ошибки, мешающие сборки приложения

--- client/src/shared/ui/Seporator/ui/Seporator.tsx
+++ client/src/shared/ui/Seporator/ui/Seporator.tsx
@@ -3,7 +3,7 @@ export const Seporator = () => {
   return (
     <div className="bg-seporator  w-full">
       <div className="max-w-[1170px] mx-auto py-[101px] flex justify-between">
-        {[1, 2, 3, 4, 5].map((elem, index) => (
+        {[1, 2, 3, 4, 5].map((_, index) => (
           <img
             key={index}
             className="max-w-[200px]"
