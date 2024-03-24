ab9fb26d43603aaad8aba2d6db602901609197c3
Kirill Anisimov <kirillanisimov1337@yandex.ru>
2024-03-24 21:13:53 +0300

    add dotEnv file

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="vite/client" />
 /// <reference path="node_modules/@2gis/mapgl/global.d.ts" />
+interface ImportMetaEnv {
+  readonly APP_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
