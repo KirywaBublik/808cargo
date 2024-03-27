45e07706a1f4043e3dcfb48638aa20bfc64797f3
Kirill Anisimov <kirillanisimov1337@yandex.ru>
2024-03-27 21:35:43 +0300

    update Readme & env ports

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,73 +1,82 @@
 import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
+
+export default defineConfig(({ mode }) => {
+  process.env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd()),
+  };
+  return {
+    plugins: [react()],
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      strictPort: true,
+      port: Number(process.env.VITE_PORT),
     },
-    host: true,
-    strictPort: true,
-    port: 5173,
-  },
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(
-          new URL("./src", import.meta.url),
-        ),
-      },
-      {
-        find: "@shared",
-        replacement: fileURLToPath(
-          new URL(
-            "./src/shared",
-            import.meta.url,
+    resolve: {
+      alias: [
+        {
+          find: "@",
+          replacement: fileURLToPath(
+            new URL("./src", import.meta.url),
           ),
-        ),
-      },
-      {
-        find: "@pages",
-        replacement: fileURLToPath(
-          new URL("./src/pages", import.meta.url),
-        ),
-      },
-      {
-        find: "@widgets",
-        replacement: fileURLToPath(
-          new URL(
-            "./src/widgets",
-            import.meta.url,
+        },
+        {
+          find: "@shared",
+          replacement: fileURLToPath(
+            new URL(
+              "./src/shared",
+              import.meta.url,
+            ),
           ),
-        ),
-      },
-      {
-        find: "@features",
-        replacement: fileURLToPath(
-          new URL(
-            "./src/features",
-            import.meta.url,
+        },
+        {
+          find: "@pages",
+          replacement: fileURLToPath(
+            new URL(
+              "./src/pages",
+              import.meta.url,
+            ),
           ),
-        ),
-      },
-      {
-        find: "@entities",
-        replacement: fileURLToPath(
-          new URL(
-            "./src/entities",
-            import.meta.url,
+        },
+        {
+          find: "@widgets",
+          replacement: fileURLToPath(
+            new URL(
+              "./src/widgets",
+              import.meta.url,
+            ),
           ),
-        ),
-      },
-      {
-        find: "@app",
-        replacement: fileURLToPath(
-          new URL("./src/app", import.meta.url),
-        ),
-      },
-    ],
-  },
+        },
+        {
+          find: "@features",
+          replacement: fileURLToPath(
+            new URL(
+              "./src/features",
+              import.meta.url,
+            ),
+          ),
+        },
+        {
+          find: "@entities",
+          replacement: fileURLToPath(
+            new URL(
+              "./src/entities",
+              import.meta.url,
+            ),
+          ),
+        },
+        {
+          find: "@app",
+          replacement: fileURLToPath(
+            new URL("./src/app", import.meta.url),
+          ),
+        },
+      ],
+    },
+  };
 });
